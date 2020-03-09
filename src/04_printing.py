be1cca6749f9f012369a69065fd546168e23efa1
Luis Floresgit config --global user.name Luis <Lflores02142@gmail.com>
2020-03-09 13:08:41 -0700

    finished with printing.py

--- src/04_printing.py
+++ src/04_printing.py
@@ -11,7 +11,11 @@ z = "I like turtles!"
 # Using the printf operator (%), print the following feeding in the values of x,
 # y, and z:
 # x is 10, y is 2.25, z is "I like turtles!"
-
+percent_op = "x=%i, y=%i, z=%s" % (x, y, z)
+print("%",percent_op)
 # Use the 'format' string method to print the same thing
-
+format_op = "x= {}, y= {}, z= {}" .format(x, y, z)
+print("form_op",format_op)
 # Finally, print the same thing using an f-string
+f_string = f"x= {x}, y= {y}, z= {z}"
+print("F", f_string)
