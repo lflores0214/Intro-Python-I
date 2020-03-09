9ff566a29cd2ac6af64490a2ecdbf4f034e9e0e3
Luis Floresgit config --global user.name Luis <Lflores02142@gmail.com>
2020-03-09 16:33:51 -0700

    finished with 10_functions.py

--- src/10_functions.py
+++ src/10_functions.py
@@ -2,11 +2,19 @@
 
 # YOUR CODE HERE
 
+
+def is_even(i):
+    if int(i) % 2 == 0:
+        return True
+
+
 # Read a number from the keyboard
 num = input("Enter a number: ")
 num = int(num)
 
 # Print out "Even!" if the number is even. Otherwise print "Odd"
-
+if is_even(num) == True:
+    print("Even!")
+else:
+    print("Odd")
 # YOUR CODE HERE
-
