e9ce33d589ece566b10aad90ce2208b4d4c90709
Luis Floresgit config --global user.name Luis <Lflores02142@gmail.com>
2020-03-09 12:39:26 -0700

    finished with 03_modules.py

--- src/03_modules.py
+++ src/03_modules.py
@@ -8,24 +8,33 @@ level operating system functionality.
 import sys
 # See docs for the sys module: https://docs.python.org/3.7/library/sys.html
 
+import os
+# See the docs for the OS module: https://docs.python.org/3.7/library/os.html
+import platform
+import socket
 # Print out the command line arguments in sys.argv, one per line:
 # YOUR CODE HERE
+print("sys.argv", sys.argv)
+
 
 # Print out the OS platform you're using:
 # YOUR CODE HERE
+print("os name", os.name)
+print("platform system", platform.system())
 
 # Print out the version of Python you're using:
 # YOUR CODE HERE
-
-
-import os
-# See the docs for the OS module: https://docs.python.org/3.7/library/os.html
+print("python version #",
+      sys.version_info[0], ".", sys.version_info[1], ".", sys.version_info[2])
 
 # Print the current process ID
 # YOUR CODE HERE
-
+print("process ID", os.getpid())
 # Print the current working directory (cwd):
 # YOUR CODE HERE
-
+print("CWD", os.getcwd())
 # Print out your machine's login name
 # YOUR CODE HERE
+import getpass
+print("socket hostname",socket.gethostname())
+print("os hostname",os.uname()[1])
