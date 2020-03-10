119f65dd57f70665c814098993d81e49e44c5fac
Luis Floresgit config --global user.name Luis <Lflores02142@gmail.com>
2020-03-10 14:04:01 -0700

    finished with 12_scopes.py

--- src/12_scopes.py
+++ src/12_scopes.py
@@ -5,11 +5,13 @@
 x = 12
 
 def change_x():
+    global x
     x = 99
 
 change_x()
 
 # This prints 12. What do we have to modify in change_x() to get it to print 99?
+# we need to add the global keyword so it knows that we are referencing the global variable
 print(x)
 
 
@@ -19,12 +21,14 @@ def outer():
     y = 120
 
     def inner():
+        nonlocal y
         y = 999
 
     inner()
 
     # This prints 120. What do we have to change in inner() to get it to print
     # 999?
+    # we need to use the nonlocal keyword so the fn knows to use the outer scope and not local scope
     # Note: Google "python nested function scope".
     print(y)
 
