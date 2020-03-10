7cbb51746de53dc9702bc8a9dfd3580a7ee77db7
Luis Floresgit config --global user.name Luis <Lflores02142@gmail.com>
2020-03-10 14:19:59 -0700

    finished with 13_file_io.py

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -4,16 +4,20 @@ at how to read and write to files here:
 
 https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 """
-
+import os
 # Open up the "foo.txt" file (which already exists) for reading
 # Print all the contents of the file, then close the file
 # Note: pay close attention to your current directory when trying to open "foo.txt"
-
+print("CWD",os.getcwd())
 # YOUR CODE HERE
-
+with open("./src/foo.txt", "r") as f:
+    print(f.read())
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
 # then close the file. Open up "bar.txt" and inspect it to make
 # sure that it contains what you expect it to contain
-
 # YOUR CODE HERE
+with open("./src/bar.txt", "w+")as b:
+    for i in range(3):
+        b.write("This should be three lines\n")
+    b.close()
